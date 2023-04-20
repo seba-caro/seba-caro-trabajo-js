04da20dda97a53a4ff1ddba734ad50108102b8b3
sebastian caro <seba-handball@outlook.com>
2023-04-20 00:18:51 -0300

    proyecto final

--- js/quienessomos.js
+++ js/quienessomos.js
@@ -3,3 +3,22 @@ window.addEventListener("load", function(){
 })
 
 
+let quote = document.getElementById("quote");
+let author = document.getElementById("author");
+let btnq = document.getElementById("btnq");
+
+const url ="https://api.quotable.io/random";
+
+let getQuote = ()=> {
+    fetch(url)
+    .then((data) =>data.json())
+    .then((item) => {
+        quote.innerText = item.content;
+        author.innerText = item.author;
+
+    })
+}
+
+window.addEventListener("load", getQuote);
+
+btnq.addEventListener("click", getQuote);
